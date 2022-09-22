cc416e5d268b396aad8fabb5931e59b3e1dbc96a
rzx100 <770304867@qq.com>
2022-09-22 19:16:32 +0800

    gisdemo

---
+++ src/views/AboutView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="about">
+    <div id="map" style="width: 100%; height: 400px"></div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { onMounted } from 'vue'
+import Map from 'ol/Map'
+import View from 'ol/View'
+import OSM from 'ol/source/OSM'
+import TileLayer from 'ol/layer/Tile'
+
+onMounted(() => {
+  const map = new Map({ target: 'map' })
+  map.setView(
+    new View({
+      center: [0, 0],
+      zoom: 2,
+    })
+  )
+  const source = new OSM()
+  const layer = new TileLayer({ source: source })
+  map.addLayer(layer)
+})
+</script>
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/LeafletView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="about">
+    <div id="map" style="width: 100%; height: 400px"></div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { onMounted } from 'vue'
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+import * as esriLeaflet from 'esri-leaflet'
+onMounted(() => {
+  const map = L.map('map').setView([51.505, -0.09], 13)
+  esriLeaflet
+    .dynamicMapLayer({
+      url: 'http://localhost:3001/_alllayers/MapServer', //地图服务地址
+      minZoom: 4, //最小缩放等级
+      f: 'image',
+      maxZoom: 21, //最大缩放等级
+      useCors: true,
+    })
+    .addTo(map)
+})
+</script>
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
