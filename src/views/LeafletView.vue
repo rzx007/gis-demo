e84f7ccf04c240c49494419331cc44c944af1624
rzx007 <770304867@qq.com>
2022-09-27 22:45:14 +0800

    feat: 为3D

--- src/views/LeafletView.vue
+++ src/views/LeafletView.vue
@@ -9,21 +9,29 @@ import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
 
 onMounted(() => {
+  const baseTile = L.tileLayer.wms('http://10.172.246.234:7070/geoserver/cite/wms?service=WMS', {
+    layers: 'cite:natural_earth,cite:china_buildings,cite:china_water,cite:china_railways',
+    transparent: true,
+    minZoom: 3,
+  })
+  const secTile = L.tileLayer.wms('http://10.172.246.234:7070/geoserver/cite/wms?service=WMS', {
+    layers: 'cite:natural_earth',
+    transparent: true,
+    minZoom: 3,
+  })
+
   const map = L.map('map', {
     crs: L.CRS.EPSG4326,
-  }).setView(L.latLng(30.58203, 114.02919), 6)
-  const baseTile = L.tileLayer.wms('http://10.172.246.234:7070/geoserver/cite/wms?service=WMS', {
-    layers: '	cite:china_buildings,cite:china_water,cite:china_railways',
-    transparent: true,
-    minZoom: 3,
+    center: L.latLng(30.57551895461493, 114.3619241258888),
+    zoom: 7,
+    layers: [baseTile],
   })
-  const secTile = L.tileLayer.wms('http://localhost:4003/geoserver/cite/wms?service=WMS', {
-    layers: 'cite:gis_osm_roads',
-    transparent: true,
-    minZoom: 3,
-  })
-  L.layerGroup([baseTile]).addTo(map)
-  const marker = L.marker(L.latLng(30.58203, 114.02919)).addTo(map)
+  const baseMaps = {
+    ['资源图']: baseTile,
+  }
+  const layerControl = L.control.layers(baseMaps).addTo(map)
+  layerControl.addBaseLayer(secTile, 'secTile')
+  const marker = L.marker(L.latLng(30.57551895461493, 114.3619241258888)).addTo(map)
   marker.bindPopup('<b>Hello world!</b><br>I am a popup.').openPopup()
   const popup = L.popup()
   function onMapClick(e: any) {
